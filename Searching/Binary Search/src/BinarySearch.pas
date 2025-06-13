111b72ac8cdad1e851a3bb6575276a2aab853aa3
Programmer-nosleep <ahmadzanisy@gmail.com>
2025-06-13 19:33:33 +0700

    update

---
+++ Searching/Binary Search/src/BinarySearch.pas
@@ -0,0 +1,35 @@
+unit BinarySearch;
+
+interface
+
+type
+  aptr = ^arr;
+  arr = array[0..128] of Integer;
+
+procedure binary_search(a: aptr; low, high, key: Integer);
+
+implementation
+
+uses
+  SysUtils;
+
+procedure binary_search(a: aptr; low, high, key: Integer);
+var
+  mid: Integer;
+begin
+  if (low <= high) then
+  begin
+    mid := (low + high) div 2;
+
+    if (a^[mid] = key) then
+      writeln(Format('Element found at index : %d', [mid]))
+    else if (key < a^[mid]) then
+      binary_search(a, low, mid - 1, key)
+    else if (a^[mid] < key) then
+      binary_search(a, mid + 1, high, key);
+  end
+  else if (low > high) then
+    writeln('Unsuccessful Search');
+end;
+
+end.
--- Searching/Binary Search/src/main.pas
+++ Searching/Binary Search/src/main.pas
@@ -1,14 +1,22 @@
-program main;
+program TestBinarySearch;
 
 uses
-  sysutils,
-  crt;
+  BinarySearch;
 
 var
-  data, a, b: Integer;
-  str : String;
+  data: arr;
+  pData: aptr;
+  i: Integer;
 
 begin
-  str := 'test';
-  writeln(str);
+  for i := 0 to 9 do
+    data[i] := i * 5; // contoh isi: 0, 5, 10, 15, ..., 45
+
+  pData := @data;
+
+  writeln('Mencari angka 25...');
+  binary_search(pData, 0, 9, 25);
+
+  writeln('Mencari angka 33...');
+  binary_search(pData, 0, 9, 33);
 end.
